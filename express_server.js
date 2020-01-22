f3d645a4196cf24cb406ecbdda1392ab696ddbd0
JehyunRa <jra4862@gmail.com>
2020-01-22 21:56:13 +0000

    wed 16:56

--- express_server.js
+++ express_server.js
@@ -28,7 +28,7 @@ const users = {
 
 // ## Function ##
 
-function generateRandomString(n) {
+const generateRandomString = function(n) {
   let result = '';
   for (let i = 0; i < n; i++) {
     let j = Math.floor(Math.random() * 3);
@@ -39,19 +39,35 @@ function generateRandomString(n) {
   return result;
 };
 
+const arrExtractSearch = function(container, extract, searchItem) {
+  arr = [];
+  exist = false;
+  subObj = undefined;
+  for (const content of Object.keys(container)) {
+    if (searchItem !== undefined) {
+      if (container[content][extract] === searchItem) {
+        subObj = content;
+        exist = true;
+      }
+    }
+    arr.push(container[content][extract]);
+  }
+  return {arr, exist, subObj};
+};
+
 // ## Register/Login/Error Page ##
 
 app.get("/register", (req, res) => {
-  let templateVars = {
-    user_id: undefined
-  };
+  let templateVars = { user_id: undefined };
+  console.log('register page: users global object:');
+  console.log(users);
   res.render("register", templateVars);
 });
 
 app.get("/login", (req, res) => {
-  let templateVars = {
-    user_id: undefined
-  };
+  let templateVars = { user_id: undefined };
+  console.log('login page: users global object:');
+  console.log(users);
   res.render("login", templateVars);
 });
 
@@ -63,20 +79,17 @@ app.get("/error/:num", (req, res) => {
   res.render("error", templateVars);
 });
 
-
 // ## Register ##
 
 app.post("/register", (req, res) => {
-  let duplicate = false;
-  for (const a in users) {
-    if (users[a].email === req.body.email) {
-      duplicate = true;
-    }
-  };
   // if the ID already exist in the server to prevent register
-  if (duplicate === true) res.redirect('/error/400');
+  if (arrExtractSearch(users, 'email', req.body.email).exist) {
+    res.redirect('/error/400');
+  }
   // if email or password is empty string
-  else if (req.body.email === '' || req.body.password === '') res.redirect('/error/400');
+  else if (req.body.email === '' || req.body.password === '') {
+    res.redirect('/error/400');
+  }
   else {
     let random = generateRandomString(10);
     users[random] = {
@@ -84,8 +97,6 @@ app.post("/register", (req, res) => {
       email: req.body.email, 
       password: req.body.password
     }
-    //test
-    console.log(users);
     res.cookie("user_id", users[random]);
     res.redirect('/urls');
   }
@@ -94,21 +105,11 @@ app.post("/register", (req, res) => {
 // ## Login ##
 
 app.post("/login", (req, res) => {
-  let duplicate = false;
-  let user_id = '';
-  for (const userID in users) {
-    if (users[userID].email === req.body.email) {
-      duplicate = true;
-      user_id = userID;
-    }
-  };
   // if the ID does not exist in the server to log into
-  if (duplicate === false) {
-    res.redirect('/error/403');
-  } else {
-    //test
-    console.log(users);
-    res.cookie("user_id", users[user_id]);
+  if (!arrExtractSearch(users, 'email', req.body.email).exist) res.redirect('/error/403');
+  else if (users[arrExtractSearch(users, 'email', req.body.email).subObj].password !== req.body.password) res.redirect('/error/403');
+  else {
+    res.cookie("user_id", arrExtractSearch(users, 'email', req.body.email).subObj);
     res.redirect('/urls');
   }
 });
@@ -123,9 +124,7 @@ app.post("/logout", (req, res) => {
 // ## CREATE ##
 
 app.get("/urls/new", (req, res) => {
-  let templateVars = {
-    user_id: undefined
-  };
+  let templateVars = { user_id: undefined };
   if (req.cookies['user_id']) {
     templateVars.user_id = req.cookies['user_id'];
     res.render("urls_new", templateVars);
@@ -134,20 +133,22 @@ app.get("/urls/new", (req, res) => {
 
 app.post("/urls", (req, res) => {
   if (req.cookies['user_id']) {
-    let duplicate = '';
-    for (const id of Object.keys(urlDatabase)) {
-      if (urlDatabase[id].longURL === req.body.longURL) {
-        duplicate = id;
-      }
-    }
+    // let duplicate = '';
+    // for (const id of Object.keys(urlDatabase)) {
+    //   if (urlDatabase[id].longURL === req.body.longURL) {
+    //     duplicate = id;
+    //   }
+    // }
     
-    if (duplicate === '') {
+
+    if (!arrExtractSearch(urlDatabase, 'longURL', req.body.longURL).subObj) {
+      // console.log(!arrExtractSearch(urlDatabase, 'longURL', req.body.longURL).subObj);
       let random = generateRandomString(6);
       //req.body.longURL === input;
       urlDatabase[random] = { longURL: req.body.longURL, userID: [] };
       urlDatabase[random].userID.push(req.cookies['user_id'].id);
       //test
-      console.log(urlDatabase);
+      // console.log(urlDatabase);
       res.redirect(`/urls/${random}`);
     } else {
       urlDatabase[duplicate].userID.push(req.cookies['user_id'].id);
